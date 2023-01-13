c385960b6fc985ada4e2302ce9173b7cd37f58fc
emmanuel <ajibodeolayinka09gmail.com>
2023-01-13 19:10:45 +0100

    first commit

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,52 @@
+import { createContext, useState } from "react"
+import { PRODUCTS } from "../products";
+import useLocalStorage from "../localStorage/useLocalStorage";
+
+export const ShopContext = createContext(null)
+
+const getDefaultCart = () => {
+   let cart = {};
+    for(let i =1; i < PRODUCTS.length +1 ; i++){
+        cart[i] = 0;
+    }
+    return cart;
+}
+ 
+export const ShopContextProvider = (props) =>{
+    // const [cartItems, setCartitems] = useLocalStorage(getDefaultCart());
+    const [cartItems, setCartitems] = useState(getDefaultCart());
+
+
+    //getTotal price of all items added to cart
+     const getTotalCartAmount =() =>{
+            let totalAmount= 0;
+            for( const item in cartItems){
+                // console.log(item)
+                if(cartItems[item] > 0){
+                   let  iteminfo = PRODUCTS.find((product) => product.id === Number(item));
+                       totalAmount += cartItems[item] * iteminfo.Price;
+                }
+    }      
+              return totalAmount;
+     };
+    
+    
+    //getting the previous amount  in the cart,then increases when user add to cart
+    const addItem = (itemId) => {
+           setCartitems((prev) => ({...prev,[itemId]:prev[itemId]+1}))
+    }
+    //getting the previous amount  in the cart,then decreases when user remobve from cart
+    const removeItem = (itemId) => {
+        setCartitems((prev) => ({...prev,[itemId]:prev[itemId]- 1}))
+ }
+  //updating the cart amount 
+  const getNewValue = (newVAlue,itemId) =>{
+          setCartitems((prev)=> ({...prev,[itemId]:newVAlue}))
+     }
+    
+    const contextValue = {cartItems,addItem,removeItem,getNewValue,getTotalCartAmount}
+
+    console.log(cartItems)
+
+    return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
+}
