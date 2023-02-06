df1e1695522d9da6f0683e3b54604e145095cbe7
emmanuel <ajibodeolayinka09gmail.com>
2023-02-07 00:57:50 +0100

    worked on the validation for the user input

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -3,44 +3,78 @@ import { useState } from "react";
 import "./signup.css";
 
 const SignUp = () => {
-  const [name, setName] = useState("");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const handleChange = (event) => {
+    const [name, value, type, checked] = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (formData.password === formData.passwordConfirm) {
+      console.log("Successfully signed up");
+    } else {
+      console.log("Passwords do not match");
+      return;
+    }
+  }
 
   return (
     <div className="box">
       <div className="registration-box">
         <h2 className="reg">Register</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <input
+              name="firstName"
               className="signupINput"
-              value={name}
+              defaultValue={formData.firstName}
               type="text"
               placeholder="First Name"
+              onChange={handleChange}
             />
             <input
+              name="lastName"
               className="signupINput"
-              value={name}
+              defaultValue={formData.lastName}
               type="text"
               placeholder="Last Name"
+              onChange={handleChange}
             />
           </div>
           <input
+            name="email"
             className="signupINput"
-            value={name}
+            defaultValue={formData.email}
             type="email"
             placeholder="Email"
+            onChange={handleChange}
           />
           <input
+            name="password"
             className="signupINput"
-            value={name}
+            defaultValue={formData.password}
             type="password"
             placeholder="Password"
+            onChange={handleChange}
           />
           <input
+            name="confirmPassword"
             className="signupINput"
-            value={name}
+            defaultValue={formData.passwordConfirm}
             type="password"
             placeholder="Confirm Password"
+            onChange={handleChange}
           />
           <div className="btn">
             <button className="signup-btn"> Create Account</button>
